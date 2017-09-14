e156e18e1e6dce419eeddcc0aa73c7b2eb9d7265
adithyahosapate <adihosapate@gmail.com>
2017-09-14 21:27:13 +0530

    restructured

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import LP
-from LP import LinearProg
-
-constraints_matrix=np.array([[1,0],[0,1]])
-objectives=np.array([[1],[0]])
-optimizing_parameters=np.array([4,5])
-lp=LinearProg(constraints_matrix,objectives,optimizing_parameters)
-lp.optimize()
